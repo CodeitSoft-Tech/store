510f5ffa1134ab20840510a604ac270d734ee2f3
CodeitSoft-Tech <45128276+CodeitSoft-Tech@users.noreply.github.com>
2020-05-09 05:12:25 -0700

    Add files via upload

---
+++ admin_area/includes/db.php
@@ -0,0 +1,17 @@
+<?php
+
+
+ $db = mysqli_connect("localhost","root","","ecom_store");
+
+ /*
+ if(!($db))
+ {
+ 	echo "Connection Fail";
+ }
+ else
+ {
+ 	echo "Connection Successful";
+ }*/
+ 
+
+?>
